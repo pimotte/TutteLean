d9c973123db0aa903bbb19e1dde7f3caf019c056
Pim Otte <p.j.otte@uu.nl>
2024-11-12 11:05:52 +0100

    Move unused result

--- TutteLean/Set.lean
+++ TutteLean/Set.lean
@@ -3,22 +3,6 @@ import TutteLean.Defs
 namespace SimpleGraph
 variable {V : Type*} {G : SimpleGraph V}
 
-lemma disjoint_pair (v w : V) (u : Set V) : Disjoint u {v , w} ↔ v ∉ u ∧ w ∉ u := by
-  constructor
-  · intro h
-    exact ⟨ (Set.disjoint_right.mp h) (Set.mem_insert v {w}), (Set.disjoint_right.mp h) (Set.mem_insert_of_mem v rfl) ⟩
-  · intro h
-    intro s hsu hsvw
-    simp only [Set.le_eq_subset, Set.bot_eq_empty] at *
-    rw [@Set.subset_def] at hsvw
-    rw [@Set.subset_empty_iff, @Set.eq_empty_iff_forall_not_mem]
-    intro x hxs
-    have := hsvw _ hxs
-    simp only [Set.mem_insert_iff, Set.mem_singleton_iff] at this
-    cases this with
-    | inl hl => exact h.1 (hsu (hl ▸ hxs))
-    | inr hr => exact h.2 (hsu (hr ▸ hxs))
-
 lemma Set.triple_ncard_two {x y z : V} (h : ({x, y, z} : Set V).ncard ≤ 2) : x = y ∨ x = z ∨ y = z := by
   by_contra! hc
   have := Set.ncard_eq_three.mpr ⟨x, y, z, hc.1, hc.2.1, hc.2.2, rfl⟩
--- notrelevant.lean
+++ notrelevant.lean
@@ -187,3 +187,18 @@ lemma evenFinsetSum {a : Finset α} (f : α → ℕ) (h : ∀ (c : a), Even (f c
   rw [this]
   simp only [Finset.sum_const_zero, Nat.zero_mod]
 
+lemma disjoint_pair (v w : V) (u : Set V) : Disjoint u {v , w} ↔ v ∉ u ∧ w ∉ u := by
+  constructor
+  · intro h
+    exact ⟨ (Set.disjoint_right.mp h) (Set.mem_insert v {w}), (Set.disjoint_right.mp h) (Set.mem_insert_of_mem v rfl) ⟩
+  · intro h
+    intro s hsu hsvw
+    simp only [Set.le_eq_subset, Set.bot_eq_empty] at *
+    rw [@Set.subset_def] at hsvw
+    rw [@Set.subset_empty_iff, @Set.eq_empty_iff_forall_not_mem]
+    intro x hxs
+    have := hsvw _ hxs
+    simp only [Set.mem_insert_iff, Set.mem_singleton_iff] at this
+    cases this with
+    | inl hl => exact h.1 (hsu (hl ▸ hxs))
+    | inr hr => exact h.2 (hsu (hr ▸ hxs))
