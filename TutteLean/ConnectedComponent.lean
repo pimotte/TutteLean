a3b6eb21d3ab254d872aa0608bb44318fbe87ca3
Pim Otte <p.j.otte@uu.nl>
2024-11-12 11:03:31 +0100

    Move to mathlib version of odd component lemma

--- TutteLean/ConnectedComponent.lean
+++ TutteLean/ConnectedComponent.lean
@@ -3,50 +3,6 @@ import TutteLean.Defs
 namespace SimpleGraph
 variable {V : Type*} {G : SimpleGraph V}
 
-
--- in #15539
-theorem ConnectedComponent.connectedComponentMk_subset {V : Type u_1} {G G' : SimpleGraph V} {v : V} (h : G ≤ G')
-  (c' : G'.ConnectedComponent) (hc' : v ∈ c'.supp) : (G.connectedComponentMk v).supp ⊆ c'.supp := by
-  intro v' hv'
-  simp only [mem_supp_iff, ConnectedComponent.eq] at hv' ⊢
-  rw [ConnectedComponent.sound (hv'.mono h)]
-  exact hc'
-
--- in #15539
-lemma ConnectedComponent.union_supps_eq_supp {G G' : SimpleGraph V}
-    (h : G ≤ G') (c' : ConnectedComponent G') : ⋃ c : {c : ConnectedComponent G | c.supp ⊆ c'.supp}, c.1.supp = c'.supp := by
-  ext v
-  simp_rw [Set.mem_iUnion]
-  refine ⟨fun ⟨i, hi⟩ ↦ i.2 hi, ?_⟩
-  intro hv
-  use ⟨G.connectedComponentMk v, by
-    simp only [Set.le_eq_subset, Set.mem_setOf_eq]
-    exact connectedComponentMk_subset h c' hv
-    ⟩
-  rfl
-
-
--- in #15539
-lemma oddSubComponent' [Fintype V] [Inhabited V] [DecidableEq V] (G G' : SimpleGraph V) [DecidableRel G.Adj] [DecidableRel G'.Adj]
-    (h : G ≤ G') (c' : ConnectedComponent G') : Odd (Nat.card c'.supp) ↔  Odd (Nat.card ({(c : {c : ConnectedComponent G | c.supp ⊆ c'.supp}) | Odd (Nat.card c.1.supp) })) := by
-  rw [Nat.card_eq_fintype_card]
-  rw [@Fintype.card_ofFinset]
-  rw [@Set.filter_mem_univ_eq_toFinset]
-  rw [@Set.toFinset_card]
-  haveI : DecidablePred (fun c : ConnectedComponent G ↦ c.supp ⊆ c'.supp) := by
-    exact Classical.decPred _
-  simp_rw [(c'.union_supps_eq_supp h).symm]
-  rw [← @Set.toFinset_card]
-  rw [@Set.toFinset_iUnion V {c : ConnectedComponent G | c.supp ⊆ c'.supp} _ _ (fun c => c.1.supp)]
-  rw [Finset.card_biUnion
-    (fun x _ y _ hxy ↦ Set.disjoint_toFinset.mpr (pairwise_disjoint_supp_connectedComponent _ (Subtype.coe_ne_coe.mpr hxy
-      )))]
-  simp_rw [Set.toFinset_card, ← Nat.card_eq_fintype_card]
-  rw [Nat.card_eq_fintype_card, Fintype.card_ofFinset]
-  apply Finset.odd_sum_iff_odd_card_odd
-
-
-
 lemma deleteAll [Fintype V] [Inhabited V] [DecidableEq V] {G : SimpleGraph V} [DecidableRel G.Adj]  (H : Subgraph G): H.deleteVerts ⊤ = ⊥ := by
   ext v w
   · rw [@Subgraph.deleteVerts_verts]
@@ -133,16 +89,16 @@ lemma oddComponentsIncrease [Fintype V] [Inhabited V] [DecidableEq V] (G G' : Si
 
             rw [@ConnectedComponent.isOdd_iff] at hc
             rw [@Fintype.card_eq_nat_card] at hc
-            have := Odd.pos <| (oddSubComponent' ((⊤ : Subgraph G).deleteVerts u).coe ((⊤ : Subgraph G').deleteVerts u).coe b c).mp hc
+            have := Odd.pos <| (ConnectedComponent.odd_card_supp_iff_odd_subcomponents ((⊤ : Subgraph G).deleteVerts u).coe b c).mp hc
             rw [@Finite.card_pos_iff] at this
             obtain ⟨ v , hv ⟩ := Classical.inhabited_of_nonempty this
 
-            use v.1
+            use v
             constructor
             · rw [@ConnectedComponent.isOdd_iff]
               rw [@Fintype.card_eq_nat_card]
-              exact hv
-            · exact v.2
+              exact hv.2
+            · exact hv.1
 
         have hb : ((⊤ : Subgraph G).deleteVerts u).coe ≤ ((⊤ : Subgraph G').deleteVerts u).coe := by
               intro x y hxy
