b93f1fa57c53ceac1d0753b1ac5a0406e4af28b5
Pim Otte <p.j.otte@uu.nl>
2025-02-04 13:29:43 +0100

    Cleanup

--- TutteLean/Clique.lean
+++ TutteLean/Clique.lean
@@ -5,57 +5,26 @@ import TutteLean.Defs
 namespace SimpleGraph
 variable {V : Type*} {G : SimpleGraph V}
 
-lemma isClique_even_iff_matches [DecidableEq V]
+-- In #18878
+theorem exists_union_disjoint_ncard_eq_of_even (he : Even s.ncard) : ∃ (t u : Set α),
+    t ∪ u = s ∧ Disjoint t u ∧ t.ncard = u.ncard := by
+  sorry
+
+lemma isClique_even_iff_matches' [DecidableEq V]
     (u : Set V) (hu : Set.Finite u) (hc : G.IsClique u) : Even u.ncard ↔ ∃ (M : Subgraph G), M.verts = u ∧ M.IsMatching := by
-  haveI : Fintype u := hu.fintype
   refine ⟨?_ , by
-    rintro ⟨M, ⟨hMl, hMr⟩⟩
-    haveI : Fintype M.verts := hMl ▸ hu.fintype
-    subst hMl
+    rintro ⟨M, ⟨rfl, hMr⟩⟩
     simpa [Set.ncard_eq_toFinset_card _ hu, Set.toFinite_toFinset,
-      Set.toFinset_card] using hMr.even_card
-    ⟩
-  intro he
-  cases' Set.eq_empty_or_nonempty u with h h
-  · subst h
-    use ⊥
-    simp only [Subgraph.verts_bot, true_and]
-    intro _ h
-    contradiction
-  · obtain ⟨x, y, ⟨hx, hy, hxy⟩⟩ := (Set.one_lt_ncard_iff hu).mp (Set.one_lt_ncard_of_nonempty_of_even hu h he)
-    let u' := u \ {x, y}
-    have : insert x (insert y (u \ {x, y})) = u := by
-      ext v
-      simp only [Set.mem_insert_iff, Set.mem_diff, Set.mem_singleton_iff]
-      rw [← or_assoc]
-      by_cases h : v = x ∨ v = y
-      · cases' h with h' h' <;> subst h' <;> simpa
-      · simp only [h, not_false_eq_true, and_true, false_or]
-    have hu'e : Even (u \ {x, y}).ncard := by
-      rw [← Set.odd_card_insert_iff (hu.diff) (by simp : y ∉ u \ {x, y})]
-      rw [← Set.even_card_insert_iff (insert y (u \ {x, y})).toFinite (by
-        simp [hxy] : x ∉ (insert y (u \ {x, y})))]
-      rwa [this]
-    have hu'c := hc.subset (Set.diff_subset : u' ⊆ u)
-    have hu'f := @hu.diff _  _ {x, y}
-    obtain ⟨M, hM⟩ := (isClique_even_iff_matches u' hu'f hu'c).mp hu'e
-    use M ⊔ subgraphOfAdj _ (hc hx hy hxy)
-    simp only [Subgraph.verts_sup, hM.1, subgraphOfAdj_verts]
-    refine ⟨by
-      rw [Set.diff_union_self]
-      exact Set.union_eq_self_of_subset_right (Set.pair_subset hx hy), ?_⟩
-    refine Subgraph.IsMatching.sup hM.2 (Subgraph.IsMatching.subgraphOfAdj (hc hx hy hxy)) ?h.hd
-    simp only [support_subgraphOfAdj, hM.2.support_eq_verts, hM.1]
-    exact Set.disjoint_sdiff_left
-termination_by u.ncard
-decreasing_by
-· simp_wf
-  refine Set.ncard_lt_ncard ?_ hu
-  exact ⟨Set.diff_subset, by
-    rw [@Set.not_subset_iff_exists_mem_not_mem]
-    use x
-    exact ⟨hx, by simp only [Set.mem_diff, Set.mem_insert_iff, Set.mem_singleton_iff, true_or,
-      not_true_eq_false, and_false, not_false_eq_true]⟩⟩
+      ← Set.toFinset_card] using @hMr.even_card _  _ _ hu.fintype⟩
+  intro h
+  obtain ⟨t, u, rfl, hd, hcard⟩ := exists_union_disjoint_ncard_eq_of_even h
+  obtain ⟨f⟩ : Nonempty (t ≃ u) := by
+    rw [← Cardinal.eq, ← t.cast_ncard (Set.finite_union.mp hu).1, ← u.cast_ncard (Set.finite_union.mp hu).2]
+    exact congrArg Nat.cast hcard
+  exact SimpleGraph.Subgraph.IsMatching.exists_of_disjoint_sets_of_equiv hd f (by
+    intro v
+    apply hc (by simp) (by simp)
+    exact hd.ne_of_mem (by simp) (by simp))
 
 lemma isNotClique_iff (G : SimpleGraph V) (u : Set V) : ¬G.IsClique u ↔ ∃ (v w : u), v ≠ w ∧ ¬ G.Adj v w := by
   constructor
